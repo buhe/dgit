f6fd3e1545f675b70a3862acdd4dbcc9416dacff
buhe <bugu1986@126.com>
2022-06-23 15:52:27 +0800

    git-u:update

--- git-remote-ipfs/src/main.rs
+++ git-remote-ipfs/src/main.rs
@@ -1,14 +1,62 @@
-use std::io::{BufReader, self, BufRead};
+use std::{ io::{self, BufRead, BufReader, Write}, process};
 
 fn main() -> std::io::Result<()> {
-    println!("Hello, world!");
+    // println!("Hello, world!");
     let mut input_handle = BufReader::new(io::stdin());
     let mut output_handle = io::stdout();
 
-    Ok(for line in input_handle.lines() {
-        let line_buf = line?;
-        match line_buf.as_str() {
-            _ => println!("{}", line_buf),
+    handle_capabilities(&mut input_handle, &mut output_handle).unwrap();
+    handle_list(&mut input_handle, &mut output_handle).unwrap();
+    // Ok(for line in input_handle.lines() {
+    //     let line_buf = line?;
+    //     match line_buf.as_str() {
+    //         _ => println!("{}", line_buf),
+    //     }
+    // })
+    Ok(())
+}
+
+fn handle_capabilities(input_handle: &mut dyn BufRead, output_handle: &mut dyn Write) -> std::io::Result<()> {
+    let mut line_buf = String::new();
+    input_handle.read_line(&mut line_buf)?;
+    match line_buf.as_str() {
+        "capabilities\n" => {
+            let response = &mut ["push"].join("\n");
+            response.push_str("\n\n");
+            output_handle.write_all(response.as_bytes())?;
         }
-    })
+        other => {
+            println!("Received unexpected command {:?}", other);
+        }
+    }
+    Ok(())
+}
+
+fn handle_list(
+    input_handle: &mut dyn BufRead,
+    output_handle: &mut dyn Write,
+) -> std::io::Result<()> {
+    let mut line_buf = String::new();
+    input_handle.read_line(&mut line_buf)?;
+
+    // Consume the command line
+    match line_buf.as_str() {
+        list if list.starts_with("list") => {
+            // trace!("Consumed the \"list*\" command");
+        }
+        // Sometimes git needs to finish early, e.g. when the local ref doesn't even exist locally
+        "\n" => {
+            // debug!("Git finished early, exiting...");
+            process::exit(0);
+        }
+        other => {
+            let msg = format!("Expected a \"list*\" command, got {:?}", other);
+            println!("{}", msg);
+        }
+    }
+
+    output_handle.write_all(b"\n")?;
+
+    
+    Ok(())
 }
