38cb98b05a93fedfe6f21857f1b7f6406c41f029
buhe <bugu1986@126.com>
2022-07-07 20:58:08 +0800

    git-u:update

--- webapp/src/pages/_layout.tsx
+++ webapp/src/pages/_layout.tsx
@@ -1,5 +1,8 @@
 import React, { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
+
+import { Breadcrumb } from 'antd';
+
 // import Footer from '../components/Footer';
 import Header from '../components/Header';
 
@@ -7,6 +10,12 @@ const Layout: React.FC = () => {
     return (
         <div>
             <Header />
+            <Breadcrumb style={{
+                paddingLeft: 20, paddingBottom: 12,paddingTop: 12
+            }}>
+                <Breadcrumb.Item>buhe</Breadcrumb.Item>
+                <Breadcrumb.Item>test-repo</Breadcrumb.Item>
+            </Breadcrumb>
             <Suspense fallback={'loading...'}>
                 <Outlet />
             </Suspense>
