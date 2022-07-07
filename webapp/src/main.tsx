4799d6c1e3a3543a18302abf2c9d18d2c6e6eb3b
buhe <bugu1986@126.com>
2022-07-07 13:18:22 +0800

    git-u:update

--- webapp/src/main.tsx
+++ webapp/src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, useRoutes } from 'react-router-dom';
 import { routes } from './routes'; // or use Vite's alias to simplify import path for nested components
+import 'antd/dist/antd.css';
 
 function App() {
   const element = useRoutes(routes);
--- webapp/src/pages/index.tsx
+++ webapp/src/pages/index.tsx
@@ -1,16 +1,52 @@
 import { Route, Link, useParams } from 'react-router-dom';
-import React from 'react';
+// import React from 'react';
 
-const component: React.FC = () => {
-    return (
-        <div>
-            <ul>
-                <li>
-                    <Link to="/App">App</Link>
-                </li>
-            </ul>
-        </div>
-    );
+// const component: React.FC = () => {
+//     return (
+//         <div>
+//             <ul>
+//                 <li>
+//                     <Link to="/App">App</Link>
+//                 </li>
+//             </ul>
+//         </div>
+//     );
+// };
+
+// export default component;
+
+import { AppstoreOutlined, MailOutlined, SettingOutlined } from '@ant-design/icons';
+import type { MenuProps } from 'antd';
+import { Menu } from 'antd';
+import React, { useState } from 'react';
+
+const items: MenuProps['items'] = [
+    {
+        label: 'Code',
+        key: 'code',
+        icon: <MailOutlined />,
+    },
+    {
+        label: (<Link to="/App">Issue</Link>),
+        key: 'issue',
+        icon: <AppstoreOutlined />,
+    },
+    {
+        label: 'Setting',
+        key: 'setting',
+        icon: <SettingOutlined />,
+    },
+];
+
+const App: React.FC = () => {
+    const [current, setCurrent] = useState('code');
+
+    const onClick: MenuProps['onClick'] = e => {
+        console.log('click ', e);
+        setCurrent(e.key);
+    };
+
+    return <Menu onClick={onClick} selectedKeys={[current]} mode="horizontal" items={items} />;
 };
 
-export default component;
+export default App;
