26f288206844572aef626aad3f35e2c294275d00
buhe <bugu1986@126.com>
2022-07-07 13:52:08 +0800

    git-u:update

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -6,9 +6,6 @@ import { reactRouterPlugin } from 'vite-plugin-next-react-router'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), reactRouterPlugin(),],
-  define: {
-    'process.env': process.env,
-    'global': {}
-  },
   resolve: { alias: { web3: path.resolve(__dirname, './node_modules/web3/dist/web3.min.js') }, }
 })
+// https://github.com/vitejs/vite/issues/2618
