fd27ec79aa5b650bfedef968b3ccac8e2ffdc7bc
buhe <bugu1986@126.com>
2022-07-07 20:30:50 +0800

    git-u:update

--- webapp/src/components/Header.tsx
+++ webapp/src/components/Header.tsx
@@ -1,9 +1,28 @@
+import { useState } from 'react';
+import Web3 from 'web3';
+import { Button } from 'antd';
 
+function App() {
+    const [account, setAccount] = useState(''); // state variable to set account.
+    const [connected, setConnected] = useState(false); // state variable to set account.
 
-const App = () => (
-    <>
-    Header
-    </>
-);
+    async function connect() {
+        try {
+            const web3 = new Web3(Web3.givenProvider || 'https://ropsten.infura.io/v3/3f433221d3db475db058b3875a617fdd');
+            setConnected(true);
+            const accounts = await web3.eth.requestAccounts();
+
+            setAccount(accounts[0]);
+        } catch (e) {
+            setConnected(false);
+        }
+    }
+
+    return (
+        <div>
+            {connected ? <Button>Disconnect {account} </Button> : <Button onClick={() => connect()}>Connect</Button>}
+        </div>
+    );
+}
 
 export default App;
