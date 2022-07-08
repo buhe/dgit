a657fd0bab7e7b96acb62408f98c64765524ce14
buhe <bugu1986@126.com>
2022-07-08 12:52:14 +0800

    git-u:update

--- webapp/src/pages/_layout.tsx
+++ webapp/src/pages/_layout.tsx
@@ -1,12 +1,45 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, useState } from 'react';
 import { Outlet } from 'react-router-dom';
 
 import { Breadcrumb } from 'antd';
+import { Route, Link, useParams } from 'react-router-dom';
+import { SettingOutlined, CodeOutlined, BugOutlined, TagOutlined } from '@ant-design/icons';
+import type { MenuProps } from 'antd';
+import { Menu } from 'antd';
 
 // import Footer from '../components/Footer';
 import Header from '../components/Header';
+const items: MenuProps['items'] = [
+    {
+        label: (<Link to="/Code">Code</Link>),
+        key: 'code',
+        icon: <CodeOutlined />,
+    },
+    {
+        label: (<Link to="/App">Issue</Link>),
+        key: 'issue',
+        icon: <BugOutlined />,
+    },
+    {
+        label: 'NFT',
+        key: 'nft',
+        icon: <TagOutlined />,
+    },
+    {
+        label: 'Setting',
+        key: 'setting',
+        icon: <SettingOutlined />,
+    },
+];
 
 const Layout: React.FC = () => {
+    const [current, setCurrent] = useState('code');
+
+    const onClick: MenuProps['onClick'] = e => {
+        console.log('click ', e);
+        setCurrent(e.key);
+    };
+
     return (
         <div>
             <Header />
@@ -16,6 +49,7 @@ const Layout: React.FC = () => {
                 <Breadcrumb.Item>buhe</Breadcrumb.Item>
                 <Breadcrumb.Item>test-repo</Breadcrumb.Item>
             </Breadcrumb>
+            <Menu onClick={onClick} selectedKeys={[current]} mode="horizontal" items={items} />
             <Suspense fallback={'loading...'}>
                 <Outlet />
             </Suspense>
--- webapp/src/pages/index.tsx
+++ webapp/src/pages/index.tsx
@@ -1,57 +1,8 @@
-import { Route, Link, useParams } from 'react-router-dom';
-// import React from 'react';
-
-// const component: React.FC = () => {
-//     return (
-//         <div>
-//             <ul>
-//                 <li>
-//                     <Link to="/App">App</Link>
-//                 </li>
-//             </ul>
-//         </div>
-//     );
-// };
-
-// export default component;
-
-import { SettingOutlined, CodeOutlined, BugOutlined, TagOutlined } from '@ant-design/icons';
-import type { MenuProps } from 'antd';
-import { Menu } from 'antd';
-import React, { useState } from 'react';
-
-const items: MenuProps['items'] = [
-    {
-        label: (<Link to="/Code">Code</Link>),
-        key: 'code',
-        icon: <CodeOutlined />,
-    },
-    {
-        label: (<Link to="/App">Issue</Link>),
-        key: 'issue',
-        icon: <BugOutlined />,
-    },
-    {
-        label: 'NFT',
-        key: 'nft',
-        icon: <TagOutlined />,
-    },
-    {
-        label: 'Setting',
-        key: 'setting',
-        icon: <SettingOutlined />,
-    },
-];
+import React from 'react';
+import Code from './Code';
 
 const App: React.FC = () => {
-    const [current, setCurrent] = useState('code');
-
-    const onClick: MenuProps['onClick'] = e => {
-        console.log('click ', e);
-        setCurrent(e.key);
-    };
-
-    return <Menu onClick={onClick} selectedKeys={[current]} mode="horizontal" items={items} />;
+    return <Code />;
 };
 
 export default App;
