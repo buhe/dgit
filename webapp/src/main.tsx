e2a6aeb57ef3b0f4947f2cfaccc7000b1b10bc8d
buhe <bugu1986@126.com>
2022-07-08 13:17:49 +0800

    git-u:update

--- webapp/src/main.tsx
+++ webapp/src/main.tsx
@@ -5,10 +5,43 @@ import { routes } from './routes'; // or use Vite's alias to simplify import pat
 import 'antd/dist/antd.css';
 import { WagmiConfig, createClient } from 'wagmi'
 import { getDefaultProvider } from 'ethers'
+import { chain, configureChains } from 'wagmi'
+import { infuraProvider } from 'wagmi/providers/infura'
+import { CoinbaseWalletConnector } from 'wagmi/connectors/coinbaseWallet'
+import { InjectedConnector } from 'wagmi/connectors/injected'
+import { MetaMaskConnector } from 'wagmi/connectors/metaMask'
+import { WalletConnectConnector } from 'wagmi/connectors/walletConnect'
 
+const infuraId = '3f433221d3db475db058b3875a617fdd'
+const { chains, provider } = configureChains(
+  [chain.ropsten],
+  [infuraProvider({ infuraId })],
+)
 const client = createClient({
   autoConnect: true,
-  provider: getDefaultProvider(),
+  connectors: [
+    new MetaMaskConnector({ chains }),
+    new CoinbaseWalletConnector({
+      chains,
+      options: {
+        appName: 'dgit',
+      },
+    }),
+    new WalletConnectConnector({
+      chains,
+      options: {
+        qrcode: true,
+      },
+    }),
+    new InjectedConnector({
+      chains,
+      options: {
+        name: 'Injected',
+        shimDisconnect: true,
+      },
+    }),
+  ],
+  provider,
 })
 
 function App() {
--- webapp/src/pages/Code.tsx
+++ webapp/src/pages/Code.tsx
@@ -1,18 +1,68 @@
 import React from 'react';
 import CodeMirror from '@uiw/react-codemirror';
 import { javascript } from '@codemirror/lang-javascript';
+import { useContractRead } from 'wagmi'
+const ABI = [
+    {
+        "inputs": [
+            {
+                "internalType": "string",
+                "name": "_greeting",
+                "type": "string"
+            }
+        ],
+        "stateMutability": "nonpayable",
+        "type": "constructor"
+    },
+    {
+        "inputs": [],
+        "name": "greet",
+        "outputs": [
+            {
+                "internalType": "string",
+                "name": "",
+                "type": "string"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "string",
+                "name": "_greeting",
+                "type": "string"
+            }
+        ],
+        "name": "setGreeting",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    }
+];
+const ADDRESS = '0x22fCB380773027B246b0EAfafC1f996938f2eF14';
 
 function App() {
+    const { data, isError, isLoading } = useContractRead({
+        addressOrName: ADDRESS,
+        contractInterface: ABI,
+        functionName: 'greet',
+    })
+
     const onChange = React.useCallback((value: any, viewUpdate: any) => {
         console.log('value:', value);
     }, []);
     return (
-        <CodeMirror
-            value="console.log('hello world!');"
-            height="200px"
-            extensions={[javascript({ jsx: true })]}
-            onChange={onChange}
-        />
+        <div>
+            <div>{data}</div>
+            <CodeMirror
+                value="console.log('hello world!');"
+                height="200px"
+                extensions={[javascript({ jsx: true })]}
+                onChange={onChange}
+            />
+        </div>
     );
 }
 export default App;
