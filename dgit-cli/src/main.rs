951d1648a82e5c084a74cb860fa96edf5a13e537
buhe <bugu1986@126.com>
2022-07-09 21:19:00 +0800

    git-u:update

--- dgit-cli/src/main.rs
+++ dgit-cli/src/main.rs
@@ -1,5 +1,41 @@
+use failure::Error;
+
 mod issue;
 mod meta;
-fn main() {
+
+use clap::{Args, Parser, Subcommand};
+
+#[derive(Parser)]
+#[clap(author, version, about, long_about = None)]
+#[clap(propagate_version = true)]
+struct Cli {
+    #[clap(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Issue management
+    Issue(Issue),
+}
+
+#[derive(Args)]
+struct Issue {
+    #[clap(value_parser)]
+    title: Option<String>,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Error>{
     println!("Hello, world!");
+    let cli = Cli::parse();
+
+    // You can check for the existence of subcommands, and if found use their
+    // matches just as you would the top level cmd
+    match &cli.command {
+        Commands::Issue(issue) => {
+            println!("'myapp add' was used, name is: {:?}", issue.title)
+        }
+    }
+    Ok(())
 }
